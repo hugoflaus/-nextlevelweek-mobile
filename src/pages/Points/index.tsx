e833b737dcf7cd7890ac65f03931b4f046f9ce70
Hugo Flausino <hugo.flausino@hotmail.com>
2020-06-11 07:59:26 -0300

    fix adjusted image_url

--- src/pages/Points/index.tsx
+++ src/pages/Points/index.tsx
@@ -20,6 +20,7 @@ interface Point {
   id: number;
   name: string;
   image: string;
+  image_url: string;
   latitude: string;
   longitude: string;
 }
@@ -131,7 +132,7 @@ const Points = () => {
             }} 
           >
             <View style={styles.mapMarkerContainer }>
-              <Image style={styles.mapMarkerImage} source={{ uri: point.image}}/>
+              <Image style={styles.mapMarkerImage} source={{ uri: point.image_url }}/>
               <Text style={styles.mapMarkerTitle}>{point.name}</Text>
             </View>
           </Marker>
